bbf41896ca6010ab71e3e528a792dfb5ddc6261a
akif ahmed <akifahmed@hotmail.com>
2023-01-02 11:35:01 +0500

    Initial Commit

---
+++ FIRSTPOD1/FIRSTPOD1.h
@@ -0,0 +1,18 @@
+//
+//  FIRSTPOD1.h
+//  FIRSTPOD1
+//
+//  Created by AkifAhmed on 02/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FIRSTPOD1.
+FOUNDATION_EXPORT double FIRSTPOD1VersionNumber;
+
+//! Project version string for FIRSTPOD1.
+FOUNDATION_EXPORT const unsigned char FIRSTPOD1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FIRSTPOD1/PublicHeader.h>
+
+
