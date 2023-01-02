6cb845aee1b7f64e049274e972b97aa37e562d4e
akif ahmed <akifahmed@hotmail.com>
2023-01-02 12:03:22 +0500

    initial commit

---
+++ FIRSTPOD1EXAMPLES/FIRSTPOD1EXAMPLESApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIRSTPOD1EXAMPLESApp.swift
+//  FIRSTPOD1EXAMPLES
+//
+//  Created by AkifAhmed on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FIRSTPOD1EXAMPLESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
