4fba059960b6530c8cad7bfb7105545bf00b88dd
akif ahmed <akifahmed@hotmail.com>
2023-01-02 12:57:05 +0500

    Added test cases

---
+++ FIRSTPOD1/FIRSTPOD1.swift
@@ -0,0 +1,23 @@
+//
+//  FIRSTPOD1.swift
+//  FIRSTPOD1
+//
+//  Created by AkifAhmed on 02/01/2023.
+//
+
+import Foundation
+
+
+public final class FirstPod1 {
+
+    let name = "FirstPod1"
+
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+
+}
--- FIRSTPOD1Tests/FIRSTPOD1Tests.swift
+++ FIRSTPOD1Tests/FIRSTPOD1Tests.swift
@@ -10,27 +10,17 @@ import XCTest
 
 final class FIRSTPOD1Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var firstPod1: FirstPod1!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+       override func setUp() {
+           firstPod1 = FirstPod1()
+       }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+       func testAdd() {
+           XCTAssertEqual(firstPod1.add(a: 1, b: 1), 2)
+       }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+       func testSub() {
+           XCTAssertEqual(firstPod1.sub(a: 2, b: 1), 1)
+       }
 }
